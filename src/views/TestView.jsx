61920cd387f6d018497867c0085c6cfc096e276f
Radek Janiszewski <janiszewski.radoslaw87@gmail.com>
2023-01-23 14:08:28 +0100

    add new view for single user editor -> temporary called TestView

---
+++ src/views/TestView.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TestView = () => {
+    return (
+        <div>
+            testview - single user editor
+        </div>
+    );
+};
+
+export default TestView;
