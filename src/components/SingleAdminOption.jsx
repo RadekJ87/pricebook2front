102b28ea797d8aecd55139436ca77b3e50911335
Radek Janiszewski <janiszewski.radoslaw87@gmail.com>
2023-01-25 14:34:12 +0100

    refactor - optimize view Options for mobile and desktop

--- src/components/SingleAdminOption.jsx
+++ src/components/SingleAdminOption.jsx
@@ -5,13 +5,16 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {Link} from "react-router-dom";
 
 
-const SingleAdminOption = ({faIcon, text, url, primaryColor ='#fafafa', secondaryColor='slateblue', ...props}) => {
+const SingleAdminOption = ({faIcon, text, url, primaryColor = '#fafafa', secondaryColor = 'slateblue', ...props}) => {
     return (
         <Paper
             elevation={3}
             component={Link}
             to={url}
             sx={{
+                fontSize: {xs: "20px", sm: "28px", md: "32px", lg: "36px"},
+                width: {xs: "180px", sm: "190px", md: "230px", lg: "280px"},
+                height: {xs: "200px", sm: "250px", md: "320px", lg: "400px"},
                 cursor: "pointer",
                 textDecoration: "none",
                 backgroundColor: `${secondaryColor}`,
@@ -21,10 +24,17 @@ const SingleAdminOption = ({faIcon, text, url, primaryColor ='#fafafa', secondar
                 alignItems: "center"
             }}>
             <Typography
-                variant="h4" sx={{color: `${primaryColor}`}}>
+                variant="h4" sx={{fontSize: "1em", color: `${primaryColor}`}}>
                 {text}
             </Typography>
-            <FontAwesomeIcon icon={faIcon} size="5x" style={{color: `${primaryColor}`, "--fa-animation-duration": "5s"}} {...props}/>
+            <FontAwesomeIcon
+                icon={faIcon}
+                // size="5x"
+                style={{
+                    fontSize: "2.35em",
+                    color: `${primaryColor}`,
+                    "--fa-animation-duration": "5s"
+                }} {...props}/>
         </Paper>
     );
 };
--- src/views/Options.jsx
+++ src/views/Options.jsx
@@ -15,22 +15,16 @@ const OptionBox = styled(Box)(({theme}) => ({
     [theme.breakpoints.up('xs')]: {
         '& > :not(style)': {
             margin: "10px",
-            width: 165,
-            height: 220,
         }
     },
     [theme.breakpoints.up('sm')]: {
         '& > :not(style)': {
             margin: "15px",
-            width: 190,
-            height: 250,
         }
     },
     [theme.breakpoints.up('md')]: {
         '& > :not(style)': {
             margin: "20px",
-            width: 280,
-            height: 400,
         }
     },
 }));
